8c3f1acc3bed72f0ccbe190fd8dbaf62876e47f2
yuuki410 <yuuki410@163.com>
2022-01-01 19:31:35 +0800

    新增一言(hitokoto.cn)插件

---
+++ plugins/hitokoto.js
@@ -0,0 +1,45 @@
+/*
+* Hitokoto.cn
+*
+* command: '!h'
+* types: [
+*     'qq/123456'
+* ]
+*/
+
+'use strict';
+
+const got = require('got');
+
+const c = {
+  "动画": "a",
+  "漫画": "b",
+  "游戏": "c",
+  "文学": "d",
+  "原创": "e",
+  "网络": "f",
+  "其他": "g",
+  "影视": "h",
+  "诗词": "i",
+  "网易云": "j",
+  "哲学": "k",
+  "抖机灵": "l",
+};
+
+module.exports = (pluginManager, options) => {
+    const bridge = pluginManager.plugins.transport;
+
+    let command = options.command || '!h';
+
+    bridge.addCommand(command, async (context) => {
+      let res;
+      if(context.param=="help"){
+        context.reply(`用法：${command} [類型（可選）：${c.keys().join('|')}]`);
+      } else if(context.param && c.keys().includes(context.param)){
+        res = await got.get(`https://v1.hitokoto.cn/?c=${c[context.param]}`).json();
+      } else {
+        res = await got.get("https://v1.hitokoto.cn/").json();
+      }
+      context.reply(`${res.hitokoto} ——${res.from_who}《${res.from}》`);
+    }, options);
+};
