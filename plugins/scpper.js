4834f7ad1ecdbc4efe4d3e488599ebc5a3dc6d6e
yuuki410 <yuuki410@163.com>
2022-01-01 15:58:44 +0800

    增加查詢SCP維基功能

---
+++ plugins/scpper.js
@@ -0,0 +1,60 @@
+/*
+* Search for SCP-Wiki
+*
+* command: '!page', '!user'
+* types: [
+*     'qq/123456'
+* ]
+*/
+
+'use strict';
+
+const format = require('string-format');
+const crom = require('./scpper/crom.js');
+
+const branch = {
+  "wl": "http://wanderers-library.wikidot.com",
+  "en": "http://scp-wiki.wikidot.com",
+  "ru": "http://scp-ru.wikidot.com",
+  "ko": "http://scpko.wikidot.com",
+  "ja": "http://scp-jp.wikidot.com",
+  "jp": "http://scp-jp.wikidot.com",
+  "fr": "http://fondationscp.wikidot.com",
+  "es": "http://lafundacionscp.wikidot.com",
+  "th": "http://scp-th.wikidot.com",
+  "pl": "http://scp-pl.wikidot.com",
+  "de": "http://scp-wiki-de.wikidot.com",
+  "cn": "http://scp-wiki-cn.wikidot.com",
+  "it": "http://fondazionescp.wikidot.com",
+  "pt": "http://scp-pt-br.wikidot.com",
+  "cs": "http://scp-cs.wikidot.com",
+  "cz": "http://scp-cs.wikidot.com",
+  "int": "http://scp-int.wikidot.com"
+};
+
+module.exports = (pluginManager, options) => {
+    const bridge = pluginManager.plugins.transport;
+
+    let command = options.command || '!search';
+
+    bridge.addCommand(command, (context) => {
+      if(context.param){
+        let res = await crom.searchPages(context.param, {
+          anyBaseUrl: !!site&&!!branch[site] ? branch[site] : branch[options.branch]
+        });
+        let ans = res.wikidotInfo ? res.wikidotInfo.title : '' ;
+        ans += ans && res.alternateTitles.length ? ' - ' : '';
+        ans += res.alternateTitles.length ? res.alternateTitles[0].title : '';
+        ans += !ans && res.translationOf && res.translationOf.wikidotInfo ? res.translationOf.wikidotInfo.title : '';
+        ans += res.wikidotInfo ? `\n評分：${res.wikidotInfo.rating}` : '' ;
+        ans += res.url ? `\n網址：${res.url}` : '';
+        if(!!ans){
+          context.reply(ans);
+        } else {
+          context.reply("無結果")
+        }
+      } else {
+        context.reply(`用法： ${command} 頁面標題`);
+      }
+    }, options);
+};
---
+++ plugins/scpper/crom.js
@@ -0,0 +1,95 @@
+/*
+ * From Zokhoi/SCPush/src/lib/crom.js
+ */
+const got = require('got');
+
+/**
+ * Interacter with Crom wiki crawler api.
+ */
+class CromApi {
+  constructor() {
+    this.base = `https://api.crom.avn.sh/graphql`
+  }
+  async req(query) {
+    return await got.post(this.base, {
+      json: {
+        query: query.trim()
+      }
+    }).json()
+  }
+
+  async searchPages(query, filter) {
+    return await this.req(`
+      {
+        searchPages(query: "${query}", filter: {
+          anyBaseUrl: ${ !!filter && !!filter.anyBaseUrl ? `"${filter.anyBaseUrl}"` : null }
+        }) {
+          url
+          wikidotInfo {
+            title
+            rating
+          }
+          alternateTitles {
+            type
+            title
+          }
+          translationOf {
+            wikidotInfo {
+              title
+              rating
+            }
+          }
+        }
+      }
+    `)
+  }
+
+  async searchUsers(query, filter) {
+    return await this.req(`
+      {
+        searchUsers(query: "${query}", filter: {
+          anyBaseUrl: ${ !!filter && !!filter.anyBaseUrl ? `"${filter.anyBaseUrl}"` : null }
+        }) {
+          name
+          authorInfos {
+            authorPage {
+              url
+            }
+          }
+          statistics${ !!filter && !!filter.baseUrl ? `(baseUrl: "${filter.baseUrl}")` : "" } {
+            rank
+            totalRating
+            meanRating
+            pageCount
+          }
+        }
+      }
+    `)
+  }
+  
+  async searchUserByRank(rank, filter) {
+    if (!(typeof rank == 'number') || isNaN(rank)) { throw new Error('Rank has to be an integer.'); }
+    return await this.req(`
+      {
+        usersByRank(rank: ${rank}, filter: {
+          anyBaseUrl: ${ !!filter && !!filter.anyBaseUrl ? `"${filter.anyBaseUrl}"` : null }
+        }) {
+          name
+          authorInfos {
+            authorPage {
+              url
+            }
+          }
+          statistics${ !!filter && !!filter.baseUrl ? `(baseUrl: "${filter.baseUrl}")` : "" } {
+            rank
+            totalRating
+            meanRating
+            pageCount
+          }
+        }
+      }
+    `)
+  }
+}
+
+module.exports = CromApi;
