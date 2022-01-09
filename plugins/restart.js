ad2b5f06dc888232d627e3df106097c3380297d3
yuuki410 <yuuki410@163.com>
2022-01-09 19:58:44 +0800

    Minor Commit

--- plugins/restart.js
+++ plugins/restart.js
@@ -20,8 +20,8 @@ module.exports = (pluginManager, options) => {
       if (operators.length && !options.operators.includes(context.from_uid)) {
         context.reply(`您沒有操作員權限，当前的操作员是${options.operators.join(', ')}`);
       } else {
-        context.reply("8秒後開始重新啟動");
-        setTimeout(()=>process.exit(0), 8000);
+        context.reply("15秒後開始重新啟動");
+        setTimeout(()=>process.exit(0), 15000);
       }
     }, options);
 };
